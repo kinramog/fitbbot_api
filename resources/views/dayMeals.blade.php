1f825cf21a569ae4b57f6499881762dde3bf6143
Kinramog <serg.gomarnik@gmail.com>
2024-05-27 20:43:26 +0300

    trying to make web-face

--- resources/views/dayMeals.blade.php
+++ resources/views/dayMeals.blade.php
@@ -1,14 +1,157 @@
+<?php
+// $wata = WaterIntake::find(1);
+// dump($wata->user->total_water_amount);
+// $users = User::find(1);
+// dd($users->waterIntakes[0]->water_amount);
+// dd("oka");
+dump($_POST);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
+
 <body>
-    <h1>
-        hey yo
-        {{$id}}
-    </h1>
+    <style>
+        body {
+            background: var(--tg-theme-bg-color);
+            color: var(--tg-theme-text-color);
+        }
+        path {
+            fill: var(--tg-theme-text-color);
+        }
+        .list-group-item {
+            background-color: var(--tg-theme-secondary-bg-color);
+            color: var(--tg-theme-text-color);
+        }
+    </style>
+
+    <div class="container" id="body">
+
+        <div id="amogus"></div>
+
+        <div class=" mt-5">
+            <h2 class="text-center mb-4">Ваш рацион за сегодня</h2>
+            <ul class="list-group">
+
+                <li class="list-group-item  rounded mb-3 shadow-sm">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="mb-2">Яблоко</h5>
+                        <span class="text-muted small">2024-05-27 08:00</span>
+                    </div>
+                    <div class="d-flex justify-content-between mb-1">
+                        <div class="d-flex flex-column">
+                            <strong>Белки:</strong>
+                            <span>0.3 г</span>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <strong>Жиры:</strong>
+                            <span>0.2 г</span>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <strong>Углеводы:</strong>
+                            <span>14 г</span>
+                        </div>
+                    </div>
+                    <hr class="my-1">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-column">
+                            <strong>Калории:</strong>
+                            <span class="font-weight-bold">52</span>
+                        </div>
+                        <button type="button" class="btn btn-sm btn-outline-secondary rounded-circle">
+                            <svg width="15px" height="15px" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="angle-down" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <path d="M33.1650874,133.072181 C43.9430028,122.416902 61.3504909,122.310349 72.2609978,132.752523 L72.589617,133.072181 L250,308.463909 L427.410383,133.072181 C438.188298,122.416902 455.595787,122.310349 466.506293,132.752523 L466.834913,133.072181 C477.612828,143.727461 477.720607,160.936879 467.15825,171.723241 L466.834913,172.048121 L269.712265,366.927819 C258.934349,377.583098 241.526861,377.689651 230.616354,367.247477 L230.287735,366.927819 L33.1650874,172.048121 C22.2783042,161.285212 22.2783042,143.83509 33.1650874,133.072181 Z" id="Shape" fill="black" fill-rule="nonzero"></path>
+                                </g>
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+
+                <li class="list-group-item  rounded mb-3 shadow-sm">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="mb-2">Куриная грудка</h5>
+                        <span class="text-muted small">2024-05-27 13:00</span>
+                    </div>
+                    <div class="d-flex justify-content-between mb-1">
+                        <div class="d-flex flex-column">
+                            <strong>Белки:</strong>
+                            <span>31 г</span>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <strong>Жиры:</strong>
+                            <span>3.6 г</span>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <strong>Углеводы:</strong>
+                            <span>0 г</span>
+                        </div>
+                    </div>
+                    <hr class="my-1">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-column">
+                            <strong>Калории:</strong>
+                            <span class="font-weight-bold">165</span>
+                        </div>
+
+                        <button type="button" class="btn btn-sm btn-outline-secondary rounded-circle">
+                            <svg width="15px" height="15px" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="angle-down" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <path d="M33.1650874,133.072181 C43.9430028,122.416902 61.3504909,122.310349 72.2609978,132.752523 L72.589617,133.072181 L250,308.463909 L427.410383,133.072181 C438.188298,122.416902 455.595787,122.310349 466.506293,132.752523 L466.834913,133.072181 C477.612828,143.727461 477.720607,160.936879 467.15825,171.723241 L466.834913,172.048121 L269.712265,366.927819 C258.934349,377.583098 241.526861,377.689651 230.616354,367.247477 L230.287735,366.927819 L33.1650874,172.048121 C22.2783042,161.285212 22.2783042,143.83509 33.1650874,133.072181 Z" id="Shape" fill="black" fill-rule="nonzero"></path>
+                                </g>
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+
+
+    </div>
+
+
+
+    <script>
+        const tg = window.Telegram.WebApp;
+        tg.ready();
+        const user_id = tg.initDataUnsafe?.user?.id;
+        const username = tg.initDataUnsafe?.user?.username;
+
+        const body = document.getElementById("body");
+        const h1 = document.createElement('h1');
+        h1.textContent = `Продукты за день ${username}`;
+        h1.classList.add("text-center", "fs-2")
+        body.appendChild(h1);
+
+        tg.MainButton.show();
+        tg.MainButton.text = "wow le button"
+
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "lol": "kek",
+            })
+        }
+
+        let data =  fetch("", requestOptions)
+            .then(response => response)
+            .then(data => console.log(data));
+
+    </script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/pupa', function () {
-    $wata = WaterIntake::find(1);    
-    dump($wata->user->total_water_amount);
-    $users = User::find(1);    
-    dd($users->waterIntakes[0]->water_amount);
-}
+        $wata = WaterIntake::find(1);
+        dump($wata->user->total_water_amount);
+        $users = User::find(1);
+        dd($users->waterIntakes[0]->water_amount);
+    }
 );
 
-Route::get('/day-meals', function () {
-    // $wata = WaterIntake::find(1);    
-    // dump($wata->user->total_water_amount);
-    // $users = User::find(1);    
-    // dd($users->waterIntakes[0]->water_amount);
-
-    return view('dayMeals', ["id" => 228]);
-}
+Route::get('/day-meals/{user_id}', function ($user_id) {
+        return view('dayMeals', ["user_id" => $user_id]);
+    }
 );
+
+// Route::any('/day-meals/', function ($request) {
+//         // $data = $request->all();
+
+//         return view('dayMeals', ["id" => 228]);
+//     }
+// );
